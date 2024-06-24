318f46b69ac01748744b017a7efb0685d9908af3
Rami <r.sahtout@student.tudelft.nl>
2024-06-24 17:47:21 +0200

    Added OGM (With sparsity)

--- Algos/Helpers.py
+++ Algos/Helpers.py
@@ -1,4 +1,8 @@
 import numpy as np
+from copy import deepcopy
+
+def frobenius_norm(A):
+    return np.linalg.norm(A, 'fro')
 
 def svd_threshold(A, threshold):
     """
@@ -114,6 +118,90 @@ def calculate_sparsity(X):
     zero_elements = np.count_nonzero(X == 0)
     sparsity = (zero_elements / total_elements) * 100
     return sparsity
+
+def compute_gradient_H(Wt, Y, X, E, beta):
+    return np.dot(np.dot(Wt.T, Wt), Y) - np.dot(Wt.T, X) + np.dot(beta, E)
+
+def compute_gradient_W(Ht, Y, X):
+    return np.dot(np.dot(Y, Ht), Ht.T) - np.dot(X, Ht.T)
+
+def OGM_calculate_objective_function(W, H, X):
+    re_er = np.linalg.norm(X - np.dot(W, H), 'fro')
+    data_norm_squared = np.linalg.norm(X, 'fro')
+    objective_percentage = (re_er / data_norm_squared) * 100
+    return objective_percentage
+
+def OGM_H(Wt, Ht, X, E, beta, max_iter=100, epsilon_h=0.01):
+    Y = deepcopy(Ht)
+    alpha = 1
+    L = np.linalg.norm(np.dot(Wt.T, Wt), 2)
+    k = 0
+    H_prev = deepcopy(Ht)
+    grad_norm_list_H = []
+
+    while k < max_iter:
+        grad_F = compute_gradient_H(Wt, Y, X, E, beta)
+        H = positive_projection(Y - (1/L) * grad_F)
+        grad_norm = frobenius_norm(positive_projection(grad_F))
+        grad_norm_list_H.append(grad_norm)
+
+        if grad_norm <= epsilon_h:
+            break
+
+        alpha_next = (1 + np.sqrt(4 * alpha**2 + 1)) / 2
+
+        if k > 0:
+            Y = H + ((alpha - 1) / alpha_next) * (H - H_prev)
+        else:
+            Y = H
+
+        alpha = alpha_next
+        H_prev = deepcopy(H)
+        k += 1
+
+    return H, grad_norm_list_H
+
+def OGM_W(Wt, Ht, X, max_iter=100, epsilon_w=0.01):
+    Y = deepcopy(Wt)
+    alpha = 1
+    L = np.linalg.norm(np.dot(Ht, Ht.T), 2)
+    k = 0
+    W_prev = deepcopy(Wt)
+    grad_norm_list_W = []
+
+    while k < max_iter:
+        grad_F = compute_gradient_W(Ht, Y, X)
+        W = positive_projection(Y - (1/L) * grad_F)
+        grad_norm = frobenius_norm(positive_projection(grad_F))
+        grad_norm_list_W.append(grad_norm)
+
+        if grad_norm <= epsilon_w:
+            break
+
+        alpha_next = (1 + np.sqrt(4 * alpha**2 + 1)) / 2
+
+        if k > 0:
+            Y = W + ((alpha - 1) / alpha_next) * (W - W_prev)
+        else:
+            Y = W
+
+        alpha = alpha_next
+        W_prev = deepcopy(W)
+        k += 1
+
+    return W, grad_norm_list_W
+
+def OGM_calculate_objective_function_observed(W, H, X, Omega):
+    re_er = np.linalg.norm(np.multiply(Omega, (X - np.dot(W, H))), 'fro')
+    data_norm_squared = np.linalg.norm(np.multiply(Omega, X), 'fro')
+    objective_percentage = (re_er / data_norm_squared) * 100
+    return objective_percentage
+
+def OGM_calculate_objective_function_unobserved(W, H, X, Omega):
+    re_er = np.linalg.norm(np.multiply((1-Omega), (X - np.dot(W, H))), 'fro')
+    data_norm_squared = np.linalg.norm(np.multiply((1-Omega), X), 'fro')
+    objective_percentage = (re_er / data_norm_squared) * 100
+    return objective_percentage
 
 def generate_simulated_IMS_matrix(m=50000, n=500, rank=100, seed=42, random_mean=1.0, random_scale=1, batch_contrast=2, batch_abondance=200, remove_batches=True):
     """
--- Algos/OGM.py
+++ Algos/OGM.py
@@ -1,124 +1,53 @@
-import numpy as np
-from copy import deepcopy
+from Algos.Helpers import OGM_calculate_objective_function, OGM_H, OGM_W, OGM_calculate_objective_function_observed, OGM_calculate_objective_function_unobserved, calculate_sparsity
 
-def frobenius_norm(A):
-    return np.linalg.norm(A, 'fro')
+def OGM_NeNMF(X, Wt, Ht, E, Sampled_mask, beta=8, max_iter=20, epsilon_h=0.01, epsilon_w=0.01, epsilon=0.001):
+    '''
+    Factorize a matrix X into two non-negative matrices W and H using the Orthogonal Gradient Method (OGM) algorithm. 
+    Algorithm based on the paper: doi: https://doi.org/10.1109/TSP.2012.2190406 
+    X: the matrix to factorize
+    Wt: the initial matrix W (randomly initialized)
+    Ht: the initial matrix H (randomly initialized)
+    E: matrix of ones with the same dimensions as Ht
+    Sampled_mask: boolean matrix of the same size as X, with True values where we have sampled (observed) the matrix X. If you use the function sample_from_matrix, you can use the third output of the function
+    beta: regularization parameter that determines the tradeoff between the sparsity of the solution and the accuracy of the factorization (higher beta means sparser solutions)
+    max_iter: maximum number of iterations
+    epsilon_h: convergence threshold for the H matrix
+    epsilon_w: convergence threshold for the W matrix
+    epsilon: convergence threshold for the algorithm
+    '''
+    iteration = 1
+    prev_error = OGM_calculate_objective_function(Wt, Ht, X)
 
-def positive_project(H):
-    return np.maximum(H, 0)
+    grad_norm_H_list = []
+    grad_norm_W_list = []
+    obj_value_list = []
+    sparsity_H_list = []
 
-#gradient H
-def compute_gradient_H(Wt, Y, X, E, beta):
-    gradient = np.dot(np.dot(Wt.T, Wt), Y) - np.dot(Wt.T, X) + np.dot(beta, E)
-    return gradient
+    while iteration < max_iter:
+        H_new, grad_norm_H = OGM_H(Wt, Ht, X, E, beta, max_iter, epsilon_h)
+        W_new, grad_norm_W = OGM_W(Wt, H_new, X, max_iter, epsilon_w)
 
-#gradient W
-def compute_gradient_W(Ht, Y, X):
-    gradient = np.dot(np.dot(Y, Ht), Ht.T) - np.dot(X,Ht.T)
-    return gradient
+        observed_error = OGM_calculate_objective_function_observed(W_new, H_new, X, Sampled_mask)
+        unobserved_error = OGM_calculate_objective_function_unobserved(W_new, H_new, X, Sampled_mask)
+        overall_error = OGM_calculate_objective_function(W_new, H_new, X)
 
-#Update algorithm H
-def OGM_H(Wt, Ht, X, E, beta, max_iter=100, epsilon_h=0.01):
-    Y = deepcopy(Ht)
-    alpha = 1
-    L = np.linalg.norm(np.dot(Wt.T, Wt), 2)
-    k = 0
-    H_prev = deepcopy(Ht)
+        obj_value_list.append((observed_error, unobserved_error, overall_error))
 
-    grad_norm_list_H = []
-
-    while k < max_iter:
-        grad_F = compute_gradient_H(Wt, Y, X, E, beta)
-        H = positive_project(Y - (1/L) * grad_F)
-
-        grad_norm = frobenius_norm(positive_project(grad_F))
-        grad_norm_list_H.append(grad_norm)
-
-        if grad_norm <= epsilon_h:
+        current_error = overall_error
+        print(current_error)
+        if prev_error - current_error <= epsilon:
             break
 
-        alpha_next = (1 + np.sqrt(4 * alpha**2 + 1)) / 2
+        prev_error = current_error
+        print(iteration)
 
-        if k > 0:
-            Y = H + ((alpha - 1) / alpha_next) * (H - H_prev)
-        else:
-            Y = H
+        Wt = W_new
+        Ht = H_new
 
-        alpha = alpha_next
-        H_prev = deepcopy(H)
-        k += 1
+        grad_norm_H_list.append(grad_norm_H[-1])
+        grad_norm_W_list.append(grad_norm_W[-1])
+        sparsity_H_list.append(calculate_sparsity(H_new))
 
+        iteration += 1
 
-
-    return H, grad_norm_list_H
-
-#Update algorithm W
-def OGM_W(Wt, Ht, X, max_iter=100, epsilon_w=0.01):
-    Y = deepcopy(Wt)
-    alpha = 1
-    L = np.linalg.norm(np.dot(Ht, Ht.T), 2)
-    k = 0
-    W_prev = deepcopy(Wt)
-
-    grad_norm_list_W = []
-
-    while k < max_iter:
-        grad_F = compute_gradient_W(Ht, Y, X)
-        W = positive_project(Y - (1/L) * grad_F)
-
-        grad_norm = frobenius_norm(positive_project(grad_F))
-        grad_norm_list_W.append(grad_norm)
-
-        if grad_norm <= epsilon_w:
-            break
-
-        alpha_next = (1 + np.sqrt(4 * alpha**2 + 1)) / 2
-
-        if k > 0:
-            Y = W + ((alpha - 1) / alpha_next) * (W - W_prev)
-        else:
-            Y = W
-
-        alpha = alpha_next
-        W_prev = deepcopy(W)
-        k += 1
-
-
-    return W, grad_norm_list_W
-
-# #Initialize parameters!!
-# n_features = data.shape[0]
-# n_samples = data.shape[1]
-# n_components = 50
-
-# X = data
-# Wt = np.random.rand(n_features, n_components)
-# Ht = np.random.rand(n_components, n_samples)
-# E = np.ones((n_components, n_samples))
-
-# t = 1
-# max_iter = 20
-# epsilon_h = 0.01
-# epsilon_w = 0.01
-# epsilon = 0.001
-# while t < max_iter:
-#     #Update H
-#     H, grad_norm_H = OGM_H(Wt, Ht, X_observed, E, beta=8)
-#     H_new = H
-
-#     #Update W
-#     W, grad_norm_W = OGM_W(Wt, H_new, X_observed)
-#     W_new = W
-
-#     #Compute errors
-#     observed_error = calculate_objective_function_observed(W_new, H_new, X, Omega)
-#     unobserved_error = calculate_objective_function_unobserved(W_new, H_new, X, Omega)
-#     overall_error = calculate_objective_function(W_new, H_new, X)
-
-#     obj_value_list.append((observed_error, unobserved_error, overall_error))
-
-#     current_error = overall_error
-#     print(current_error)
-#     #Convergence criteria
-#     if prev_error - current_error <= epsilon:
-#       break
+    return Wt, Ht, grad_norm_H_list, grad_norm_W_list, obj_value_list, sparsity_H_list
